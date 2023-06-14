eb580547de12c743680a6613b5eeb430a5263b40
GlazHorsez <to5gogol@gmail.com>
2023-06-14 17:31:05 +0300

    task 60

---
+++ task60/Program.cs
@@ -0,0 +1,26 @@
+﻿int [,,]Get3DMtx(){
+    int [,,]mtx=new int [4,4,4];
+    int n=10;
+    for(int i=0;i<mtx.GetLength(0);i++){
+        for(int j=0;j<mtx.GetLength(1);j++){
+            for (int k=0;k<mtx.GetLength(2);k++){
+                mtx[i,j,k]=n;
+                n++;
+            }
+        }
+    }
+    return mtx;
+}
+
+void ShowMtx(int [,,]mtx){
+    for(int i=0;i<mtx.GetLength(0);i++){
+        for(int j=0;j<mtx.GetLength(1);j++){
+            for (int k=0;k<mtx.GetLength(2);k++){
+            Console.Write($"{mtx[i,j,k]}({i},{j},{k})\t");
+            }
+        Console.WriteLine();
+        }
+    }
+}
+int [,,]mtx=Get3DMtx();
+ShowMtx(mtx);
